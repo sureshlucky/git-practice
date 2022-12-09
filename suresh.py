bf41cbc47d7be938348dfe3ddfe1040fbcd8fe7f
soori <ssuresh151988@gmail.com>
2022-12-09 20:43:47 +0530

    second commit

--- suresh.py
+++ suresh.py
@@ -1,5 +1,5 @@
-n = int(input("Enter n vale "))
+n = int(input("Enter n vale ::"))
 if n %2 ==0:
     print(n,"is a even number")
 else:
-    print((n,"is a odd number")
+    print(n,"is a odd number")
