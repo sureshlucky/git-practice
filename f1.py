854c8952abfdf3dafb6cb004d889744df90d6e63
jashwin <ummaisettysuresh2@gmail.com>
2022-12-20 19:00:42 +0530

    added new line

--- f1.py
+++ f1.py
@@ -1,1 +1,9 @@
-print("Hellow World")
+print("Learning  python")
+print("my name is suresh")
+print("Working in TCS")
+print("newline")
+print("Working in TCS")
+print("Added this line in master")
+
+
+
